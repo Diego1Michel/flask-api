6b47451a68083d79ea2ab83b35e28aaeb7668f48
Diego1Michel <diego.michel859@gmail.com>
2022-07-19 21:55:31 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__) #incializa a aplicação
+
+#criar uma lista com dicionário e lojas e itens
+lojas = [
+    {
+        'name':'Magazine',
+        'itens':[
+            {
+                'name': 'Jaqueta',
+                'price': 300
+            }
+        ]
+    }
+]
+
+#post  /loja/ data: {name :} - cria uma nova loja
+@app.route(' /loja', methods=['POST'])
+def cria_loja():
+    pass
+
+@app.route(' /loja')
+def get_lojas():
+
